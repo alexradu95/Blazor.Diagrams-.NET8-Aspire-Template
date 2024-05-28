3fe4a7202430ab76f8d9991e2a871ae881c75f80
Radu Alexandru <alexradu95@proton.me>
2024-05-28 21:02:39 +0300

    fix

--- NazarAspire.Web/Program.cs
+++ NazarAspire.Web/Program.cs
@@ -12,13 +12,6 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddOutputCache();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
-
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
---
+++ NazarAspire.Web/prompt.md
@@ -0,0 +1,1 @@
+Node Based Application written in Blazor.
